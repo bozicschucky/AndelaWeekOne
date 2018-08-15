b8d31340edef6db5c612e078eaa6aaf620991249
sekito charlse <bozicslxye1@gmail.com>
2018-08-15 22:20:58 +0200

    [chore #159804212] set up unit test config

--- API/test_api.py
+++ API/test_api.py
@@ -3,3 +3,16 @@ import unittest
 import json
 
 from API import app
+
+
+class APITestCase(unittest.TestCase):
+    """Unit testing class API"""
+
+    def setUp(self):
+        ''' Intial variable to use for the app '''
+        self.app = app
+        self.client = self.app.test_client
+        self.mock_data = {
+            "_id": 0,
+            "question": "How do i fix bug x"
+        }
