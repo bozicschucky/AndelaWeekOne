aa511e5f1ff748c2a336721e964b610c68f55e68
sekito charlse <bozicslxye1@gmail.com>
2018-08-15 23:19:37 +0200

    [feature #159804817] add an answer to a specific question

--- API/routes.py
+++ API/routes.py
@@ -41,3 +41,12 @@ class Question(Resource):
     def delete(self, _id):
         '''Delete a certain resource/question given an id'''
         return HANDLER.delete(_id)
+
+
+@api.route('/questions/<int:_id>/answers')
+class QuestionsReply(Resource):
+    """Reply to a specific question"""
+    @api.expect(ANSWER)
+    def post(self, _id):
+        '''Get a question and reply to it with an Answer '''
+        return HANDLER.answer_question(_id, api.payload), 200
