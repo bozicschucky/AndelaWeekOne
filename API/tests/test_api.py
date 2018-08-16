8d13f3976eb63b61eddc7270cfe6f978fce1d286
sekito charlse <bozicslxye1@gmail.com>
2018-08-16 15:50:01 +0200

    [chore #159804212] update tests to match the changes made to the api

--- API/tests/test_api.py
+++ API/tests/test_api.py
@@ -13,7 +13,7 @@ class APITestCase(unittest.TestCase):
         self.app = app
         self.client = self.app.test_client
         self.mock_data = {
-            "_id": 0,
+            "_id": 1,
             "question": "How do i fix bug x"
         }
         self.answer = {
@@ -25,22 +25,21 @@ class APITestCase(unittest.TestCase):
         res = self.client().post('/api/v1/questions',
                                  data=json.dumps(self.mock_data),
                                  content_type='application/json')
-        self.assertEqual(res.status_code, 200)
-        self.assertIn('How do i fix bug x', str(res.data))
+        self.assertEqual(res.status_code, 201)
+        self.assertIn('1', str(res.data))
 
     def test_can_create_answer_to_question(self):
         ''' Test if a user can create an answer to a  question '''
         res = self.client().post('api/v1/questions/1/answers',
                                  data=json.dumps(self.answer),
                                  content_type='application/json')
-        print(res.data)
         self.assertEqual(res.status_code, 200)
 
     def test_get_all_questions(self):
         res = self.client().post('/api/v1/questions',
                                  data=json.dumps(self.mock_data),
                                  content_type='application/json')
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 201)
         response = self.client().get('/api/v1/questions')
         self.assertEqual(response.status_code, 200)
         self.assertIn('How do i fix bug x', str(response.data))
@@ -49,7 +48,7 @@ class APITestCase(unittest.TestCase):
         res = self.client().post('/api/v1/questions',
                                  data=json.dumps(self.mock_data),
                                  content_type='application/json')
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 201)
         response = self.client().get('/api/v1/questions/1')
         self.assertEqual(response.status_code, 200)
         self.assertIn('How do i fix bug x', str(response.data))
