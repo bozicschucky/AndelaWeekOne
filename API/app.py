763b5be301866eb6e359b10750b9112f6929ae61
sekito charlse <bozicslxye1@gmail.com>
2018-08-21 06:31:24 +0200

    [chore #159875590] remove test config from app

--- API/app.py
+++ API/app.py
@@ -4,7 +4,6 @@ from flask_restplus import  Api
 app = Flask(__name__, instance_relative_config=True)
 # app.config.from_object('config')
 # app.config.from_pyfile('config.py')
-app.config['TESTING'] = True
 app.config['DEBUG'] = True
 
 api = Api(app, prefix='/api/v1', version='1.0',
