0fa8f90c296356f1ee8c8cae796390e5d2e3bea2
sekito charlse <bozicslxye1@gmail.com>
2018-08-23 10:43:50 +0200

    [chore #159875590] update tests to match oop feedback changes

--- coreapi/tests/test_api.py
+++ coreapi/tests/test_api.py
@@ -15,10 +15,13 @@ class APITestCase(unittest.TestCase):
         self.client = self.app.test_client
         self.mock_data = {
             "_id": 1,
-            "question": "How do i fix bug x"
+            "question_title": "How do i work with python",
+            "question_body": "I am facing bug x"
         }
         self.answer = {
-            'answer': 'I am answering to question 1'
+            "Answer_title": "Use pep 8",
+            "Answer_body": "Pep 8 and pylint help fix bugs",
+            "Accept_status": False
         }
 
     def test_can_create_question(self):
@@ -28,6 +31,8 @@ class APITestCase(unittest.TestCase):
                                  content_type='application/json')
         self.assertEqual(res.status_code, 201)
         self.assertIn('1', str(res.data))
+        self.assertIn('How do i work with python', str(res.data))
+        self.assertIn('I am facing bug x', str(res.data))
 
     def test_can_create_answer_to_question(self):
         ''' Test if a user can create an answer to a  question '''
@@ -35,6 +40,7 @@ class APITestCase(unittest.TestCase):
                                  data=json.dumps(self.answer),
                                  content_type='application/json')
         self.assertEqual(res.status_code, 201)
+        # self.assertEqual()
 
     def test_get_all_questions(self):
         res = self.client().post('/api/v1/questions',
@@ -43,20 +49,20 @@ class APITestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 201)
         response = self.client().get('/api/v1/questions')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('How do i fix bug x', str(response.data))
+        self.assertIn('How do i work with python', str(response.data))
 
     def test_can_get_one_question(self):
         res = self.client().post('/api/v1/questions',
                                  data=json.dumps(self.mock_data),
                                  content_type='application/json')
         self.assertEqual(res.status_code, 201)
-        response = self.client().get('/api/v1/questions/1')
+        response = self.client().get('/api/v1/questions/2')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('How do i fix bug x', str(response.data))
+        self.assertIn('How do i work with python', str(response.data))
 
-    def tearDown(self):
-        '''teardown configs after running tests '''
-        pass
+    def test_can_delete_a_question(self):
+        response = self.client().delete('/api/v1/questions/1')
+        self.assertEqual(response.status_code, 204)
 
 
 if __name__ == '__main__':
