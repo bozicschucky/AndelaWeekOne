7acc11d9946a2a9b43c0d5f7b8575f1125d00a78
sekito charlse <bozicslxye1@gmail.com>
2018-08-23 08:53:08 +0200

    [chore #159875590] remove redundant feild

--- coreapi/routes.py
+++ coreapi/routes.py
@@ -6,14 +6,14 @@ from .models import DbHandler
 
 handler = DbHandler()
 
+
 question = api.model('Question', {
     '_id': fields.Integer(readOnly=True,
                           description='Question id', min=8),
     'question_title': fields.String(required=True,
                                     description='The question title'),
     'question_body': fields.String(required=True,
-                                   description='The question details'),
-    'date_time': fields.DateTime()
+                                   description='The question details')
 })
 
 answer = api.model('Answer', {
@@ -39,7 +39,8 @@ class AllQuestions(Resource):
     @api.marshal_with(question, skip_none=True, code=201)
     def post(self):
         """ Create a specific question """
-        return handler.create(api.payload), 201
+        data = api.payload
+        return handler.create(data), 201
 
 
 @api.route('/questions/<int:_id>')
@@ -53,7 +54,7 @@ class Question(Resource):
     def delete(self, _id):
         '''Delete a certain resource/question given an id'''
         handler.delete(_id)
-        return '', 204
+        return {'message': 'question {} deleted'.format(_id)}, 204
 
 
 @api.route('/questions/<int:_id>/answers')
@@ -63,4 +64,6 @@ class QuestionsReply(Resource):
     def post(self, _id):
         '''Get a question and reply to it with an Answer '''
         data = handler.add_items(api.payload)
-        return handler.answer_question(_id, data), 201
+        # return handler.answer_question(_id, data), 201
+        handler.answer_question(_id, data)
+        return {'message': 'answer created for  question {}'.format(_id)}, 201
