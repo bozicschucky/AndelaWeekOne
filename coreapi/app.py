f77cc0a3da34e1a9b1cf484a0ec404d43a8570cc
sekito charlse <bozicslxye1@gmail.com>
2018-08-24 08:56:39 +0200

    [chore #159875590] refactor app validation

--- coreapi/app.py
+++ coreapi/app.py
@@ -5,6 +5,7 @@ app = Flask(__name__, instance_relative_config=True)
 # app.config.from_object('config')
 # app.config.from_pyfile('config.py')
 app.config['DEBUG'] = True
+app.config['RESTPLUS_VALIDATE'] = True
 
 api = Api(app, prefix='/api/v1', version='1.0',
           title='StackoverFlowLite', description='Q and A api')
